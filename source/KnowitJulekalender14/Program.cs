39efcae6e38e595627a9c1ed756c11e9c81895aa
Terje Wiesener <terje.wiesener@sonat.no>
2019-12-17 20:47:22 +0100

    Luke 14 og 15

---
+++ source/KnowitJulekalender14/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace KnowitJulekalender14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Optimized();
+            }
+            //Naive();
+        }
+
+        public static unsafe void Optimized()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            byte[] alphabet = { 2, 3, 5, 7, 11 };
+            int sequenceLength = 217532235;
+            double averageSize = alphabet.Average(b => (int)b);
+            int requiredMem = (int)(sequenceLength / (averageSize * 0.99)); //ta høyde for 1% avvik fra snittet
+            int stopWriting = requiredMem - alphabet.Max();
+
+
+            byte* sequenceptr = (byte*)Marshal.AllocHGlobal(requiredMem * sizeof(byte));
+            int alphabetLength = alphabet.Length;
+
+            int counter = 0;
+
+            sequenceptr[0] = alphabet[0];
+            sequenceptr[1] = alphabet[0];
+            counter++;
+            int end = 2;
+
+            int valueIndex = 1;
+            int numSevens = 0;
+            {
+                fixed (byte* alphabetptr = alphabet)
+                {
+                    while (end < stopWriting)
+                    {
+                        var num = (int)sequenceptr[counter];
+                        byte value = alphabetptr[valueIndex];
+
+                        byte* target = sequenceptr + end;
+
+                        if (num == 7) goto WRITE7;
+                        if (num == 5) goto WRITE5;
+                        if (num == 3) goto WRITE3;
+                        if (num == 2) goto WRITE2;
+                        *(target + 10) = value;
+                        *(target + 9) = value;
+                        *(target + 8) = value;
+                        *(target + 7) = value;
+                    WRITE7:
+                        *(target + 6) = value;
+                        *(target + 5) = value;
+                    WRITE5:
+                        *(target + 4) = value;
+                        *(target + 3) = value;
+                    WRITE3:
+                        *(target + 2) = value;
+                    WRITE2:
+                        *(target + 1) = value;
+                        *target = value;
+                        end += num;
+                        if (value == 7) numSevens += num;
+                        counter++;
+                        valueIndex++;
+                        if (valueIndex >= alphabetLength) valueIndex = 0;
+                    }
+                    while (end < sequenceLength)
+                    {
+                        var num = (int)sequenceptr[counter];
+                        byte value = alphabetptr[valueIndex];
+
+                        end += num;
+                        if (value == 7) numSevens += num;
+                        counter++;
+                        valueIndex++;
+                        if (valueIndex >= alphabetLength) valueIndex = 0;
+                    }
+                }
+            }
+
+            var elapsed = sw.Elapsed;
+            Console.WriteLine($"Sum av syvere i sekvensen er {numSevens * 7} (brukte {elapsed.TotalMilliseconds} ms)");
+        }
+
+        public static void Naive()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            byte[] sequence = new byte[217532235];
+            byte[] alphabet = { 2, 3, 5, 7, 11 };
+            int alphabetLength = alphabet.Length;
+
+            int counter = 0;
+
+            sequence[0] = alphabet[0];
+            sequence[1] = alphabet[0];
+            counter++;
+            int end = 2;
+
+            int valueIndex = 1;
+            int numSevens = 0;
+            while (end < sequence.Length)
+            {
+                var num = (int)sequence[counter];
+                byte value = alphabet[valueIndex];
+                for (int i = 0; i < num; i++)
+                {
+                    sequence[end + i] = value;
+                }
+                end += num;
+                if (value == 7) numSevens += num;
+                counter++;
+                valueIndex++;
+                if (valueIndex >= alphabetLength) valueIndex = 0;
+            }
+
+            var sum = numSevens * 7;
+            var elapsed = sw.Elapsed;
+            Console.WriteLine($"Sum av syvere i sekvensen er {sum} (brukte {elapsed.TotalMilliseconds} ms)");
+        }
+    }
+}
---
+++ source/KnowitJulekalender15/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+
+namespace KnowitJulekalender15
+{
+    public class Vector
+    {
+        public float X;
+        public float Y;
+        public float Z;
+        public IEnumerable<float> Components 
+        { 
+            get
+            {
+                yield return X;
+                yield return Y;
+                yield return Z;
+            }
+        }
+
+        public string ToObj()
+        {
+            return "v " + string.Join(" ", Components.Select(c => c.ToString(CultureInfo.InvariantCulture)));
+        }
+    }
+
+    public class Triangle
+    {
+        public Vector P1;
+        public Vector P2;
+        public Vector P3;
+        public IEnumerable<Vector> Coordinates
+        {
+            get
+            {
+                yield return P1;
+                yield return P2;
+                yield return P3;
+            }
+        }
+
+        public Triangle(string line)
+        {
+            var components = line.
+                Split(",", StringSplitOptions.RemoveEmptyEntries).
+                Select(c => float.Parse(c, CultureInfo.InvariantCulture)).
+                ToArray();
+
+            P1 = new Vector()
+            {
+                X = components[0],
+                Y = components[1],
+                Z = components[2],
+            };
+            P2 = new Vector()
+            {
+                X = components[3],
+                Y = components[4],
+                Z = components[5],
+            };
+            P3 = new Vector()
+            {
+                X = components[6],
+                Y = components[7],
+                Z = components[8],
+            };
+        }
+
+        public string ToObj()
+        {
+            var coords = string.Join("\n", Coordinates.Select(c => c.ToObj()));
+            return coords + "\nf -3 -2 -1\n";
+        }
+
+        public double SignedVolume()
+        {
+            var v321 = P3.X * P2.Y * P1.Z;
+            var v231 = P2.X * P3.Y * P1.Z;
+            var v312 = P3.X * P1.Y * P2.Z;
+            var v132 = P1.X * P3.Y * P2.Z;
+            var v213 = P2.X * P1.Y * P3.Z;
+            var v123 = P1.X * P2.Y * P3.Z;
+            return (1.0f / 6.0f) * (-v321 + v231 + v312 - v132 - v213 + v123);
+        }
+    }
+
+    public class Model
+    {
+        public Model(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null) {
+                    Triangles.Add(new Triangle(line));
+                }
+            }
+        }
+
+        public string ToObj()
+        {
+            var triangles = string.Join("\n", Triangles.Select(t => t.ToObj()));
+            return triangles;
+        }
+
+        public List<Triangle> Triangles = new List<Triangle>();
+
+        public double Volume()
+        {
+            var signedVolume = Triangles.AsParallel().Sum(t => t.SignedVolume());
+            return Math.Abs(signedVolume);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            string path = Path.GetFullPath("./model.csv.zip");
+            using (var archive = ZipFile.OpenRead(path))
+            {
+                foreach (var entry in archive.Entries)
+                {
+                    var model = new Model(entry.Open());
+                    var volume = model.Volume();
+                    var elapsed = sw.Elapsed;
+                    Console.WriteLine($"Volume of model is {volume / 1000} (spent {elapsed.TotalMilliseconds} ms)");
+
+                    //var obj = model.ToObj();
+                    //Console.WriteLine(obj);
+                }
+            }
+        }
+    }
+}
