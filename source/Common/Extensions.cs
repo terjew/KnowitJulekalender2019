61f0352fa6ec45d934291ad6f19d6db59be4cc04
Terje Wiesener <terje.wiesener@sonat.no>
2019-12-18 11:29:39 +0100

    Luke 18

---
+++ source/Common/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public static class Extensions
+    {
+        public static IEnumerable<IEnumerable<T>> Split<T>(this T[] array, T separator)
+        {
+            var comparer = EqualityComparer<T>.Default;
+
+            int start = 0;
+            while (start < array.Length)
+            {
+                int count = 0;
+                while (start + count < array.Length && !comparer.Equals(array[start + count], separator))
+                {
+                    count++;
+                }
+                yield return array.Skip(start).Take(count);
+                start += count + 1;
+            }
+
+        }
+    }
+}
